0d8c1bac698cc45acd08e9cd0cc0932232fc7bb3
Villasanti Francisco <villasanti.francisco@gmail.com>
2014-04-17 04:23:50 -0300

    first restful request

--- dashboard.js
+++ dashboard.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-
 	$("#listaAlu").click(function (event){
 		showControlPanel();
 	});
@@ -9,32 +8,79 @@ $(document).ready(function(){
 
 
 	});
+
+	$("#formListaAlu").submit(function( event ){
+		event.preventDefault();
+ 		$("#loaderDiv").fadeIn("fast");
+ 		var params = {
+ 			fechaconsulta: $("#fechaInput").val().toString(),
+ 			suc : "1"
+ 		};
+
+ 		getListaAlu(params);
+	});
 });
 
 function showControlPanel(){
 		event.preventDefault();
-			alert("entra");
 		$("#homePan").fadeOut("fast");
 		$("#ctrlListaAlu").fadeIn("slow");
 		$("#visor").fadeIn("slow");
-
 }
 
 
 function getListaAlu(params){
 
+$.get("/jasperserver/rest_v2/reports/reports/ListaAlumnos/Listado_de_Alumnos.pdf",params,function( data ){
+
+		console.log(data);
+			PDFJS.getDocument(data).then(function(pdf) {
+  // Using promise to fetch the page
+  pdf.getPage(1).then(function(page) {
+    var scale = 1.5;
+    var viewport = page.getViewport(scale);
+
+    //
+    // Prepare canvas using PDF page dimensions
+    //
+    var canvas = document.getElementById('visor');
+    var context = canvas.getContext('2d');
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+
+    //
+    // Render PDF page into canvas context
+    //
+    var renderContext = {
+      canvasContext: context,
+      viewport: viewport
+    };
+    page.render(renderContext);
+  });
+});
+
+			 		$("#loaderDiv").fadeOut("fast");
+
+
+});
+/*
 		$.ajax ({
 		type: "GET",
-		url: "/jasperserver/rest_v2/reports",
+		url: "/jasperserver/rest_v2/reports/reports/ListaAlumnos/Listado_de_Alumnos.pdf",
 		//contentType: "application/json; charset=utf-8",
-		dataType: "json",
+		dataType: "xml",
 		data: params,
 		success: function (data, status, jqXHR) {
-			darRespuesta(data.status);
+			alert("exito! "+data);
+			//darRespuesta(data.status);
 		},
 		error: function(xhrequest, ErrorText, thrownError) {
             mostrarError(thrownError + ": " + ErrorText);
         }
 
+	}).done(function(){
+		 		$("#loaderDiv").fadeOut("fast");
+
 	});
+*/
 }
