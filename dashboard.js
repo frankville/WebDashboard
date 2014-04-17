1ea57b984c422bae2a71d05d6ace1db5216f045a
Villasanti Francisco <villasanti.francisco@gmail.com>
2014-04-17 18:57:23 -0300

    done pdf fetching from server

--- dashboard.js
+++ dashboard.js
@@ -28,59 +28,24 @@ function showControlPanel(){
 		$("#visor").fadeIn("slow");
 }
 
+function hideLoadingBar(){
+	$("#loaderDiv").fadeOut("fast");
+}
+
 
 function getListaAlu(params){
-
+/*
 $.get("/jasperserver/rest_v2/reports/reports/ListaAlumnos/Listado_de_Alumnos.pdf",params,function( data ){
-
-		console.log(data);
-			PDFJS.getDocument(data).then(function(pdf) {
-  // Using promise to fetch the page
-  pdf.getPage(1).then(function(page) {
-    var scale = 1.5;
-    var viewport = page.getViewport(scale);
-
-    //
-    // Prepare canvas using PDF page dimensions
-    //
-    var canvas = document.getElementById('visor');
-    var context = canvas.getContext('2d');
-    canvas.height = viewport.height;
-    canvas.width = viewport.width;
-
-    //
-    // Render PDF page into canvas context
-    //
-    var renderContext = {
-      canvasContext: context,
-      viewport: viewport
-    };
-    page.render(renderContext);
-  });
-});
-
+	var url = "jasperserver/rest_v2/reports/reports/ListaAlumnos/Listado_de_Alumnos.pdf?fechaconsulta="+params.fechaconsulta+"&suc="+params.suc;
+		console.log(url);
+			$("#visor").attr("src","/Viewer.js/../"+url);
 			 		$("#loaderDiv").fadeOut("fast");
 
 
 });
-/*
-		$.ajax ({
-		type: "GET",
-		url: "/jasperserver/rest_v2/reports/reports/ListaAlumnos/Listado_de_Alumnos.pdf",
-		//contentType: "application/json; charset=utf-8",
-		dataType: "xml",
-		data: params,
-		success: function (data, status, jqXHR) {
-			alert("exito! "+data);
-			//darRespuesta(data.status);
-		},
-		error: function(xhrequest, ErrorText, thrownError) {
-            mostrarError(thrownError + ": " + ErrorText);
-        }
-
-	}).done(function(){
-		 		$("#loaderDiv").fadeOut("fast");
-
-	});
 */
+	var url = "jasperserver/rest_v2/reports/reports/ListaAlumnos/Listado_de_Alumnos.pdf?fechaconsulta="+params.fechaconsulta+"&suc="+params.suc;
+	$("#visor").attr("src","/Viewer.js/../"+url);
+
+
 }
